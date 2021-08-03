4b11d794b63f23bac349a3eb864e68a4b5612d90
helia1385mir <88113615+helia1385mir@users.noreply.github.com>
2021-08-03 14:03:07 +0430

    Add files via upload

---
+++ tas.py
@@ -0,0 +1,10 @@
+import random
+while 1 :
+    number=random.randint(1,6)
+    if number==6 :
+        print('6.very well')
+        jayeze=random.randint(1,6)
+        print(jayeze)
+        break
+    else:
+        print(number)
